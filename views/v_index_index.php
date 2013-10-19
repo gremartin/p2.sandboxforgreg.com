84d257d28e327060a0884764bebca15a7f2fa89b
Greg Martin <gremartin@yahoo.com>
2013-10-18 21:39:41 -0400

    made edits to index view

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,3 @@
-<p>
-	Hello World! You have successfully spawned a new application.
-</p>
-
-<p>
-	This message is being triggered via the c_index.php controller, within the index() method.
-</p>
-
-<p>
-	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
-</p>
+<?php ?>
+<h1>Welcome to Terse Words</h1>
+<p>Please sign up or log in</p>
