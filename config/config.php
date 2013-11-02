a0644f5f0aa6f73ac4b870bb78b2dc190a6b242f
Greg Martin <gremartin@yahoo.com>
2013-11-01 22:11:58 -0400

    changed database name in config file

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_sandboxforgreg_com'); 
+	define('DB_NAME', 'sandboxf_p2_sandboxforgreg_com'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
