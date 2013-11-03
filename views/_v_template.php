eaf480edf7dadd884f3af0eba8a617020893ee09
Greg Martin <gremartin@yahoo.com>
2013-11-03 16:10:25 -0500

    Including master template meant for last commit

--- views/_v_template.php
+++ views/_v_template.php
@@ -14,25 +14,29 @@
 <body>	
 	
 	<div id="page">
-		<div id ='menu'>
-			
-			<?php if($user) echo 'Hello, '.$user->first_name.'!'?>
+		
+		<div id ='header'>			
+			<a href ='/'><img src = "../images/tersewords-logo.gif" alt="Terse Words" id = 'logo'/></a>
+			<div id = 'menu'>	
+				<?php if($user) echo 'Hello, '.$user->first_name.'!'?>
+					
+				<a href='/'>Home</a>
 				
-			<a href='/'>Home</a>
-			
-			<!-- Menu for logged in users -->
-			<?php if($user): ?>
-				<a href='/posts/add'>Post</a>
-				<a href='/posts/index'>View Posts</a>
-				<a href='/users/logout'>Logout</a>
-				<a href='/users/profile'>Profile</a>
-			<?php else: ?>
-			<!-- Menu for users not logged in -->
-				<a href='/users/signup'>Sign up</a>
-				<a href='/users/login'>Log in</a>
-				
-			<?php endif; ?>
-		</div>
+				<!-- Menu for logged in users -->
+				<?php if($user): ?>
+					<a href='/posts/add'>Post</a>
+					<a href='/posts/index'>View Posts</a>
+					<a href='/posts/users'>View Users</a>
+					<a href='/users/logout'>Logout</a>
+					<a href='/users/profile'>Profile</a>
+				<?php else: ?>
+				<!-- Menu for users not logged in -->
+					<a href='/users/signup'>Sign up</a>
+					<a href='/users/login'>Log in</a>
+					
+				<?php endif; ?>
+			</div>  <!-- end menu div-->
+		</div>	<!--end header div-->
 		
 		<br>
 		
